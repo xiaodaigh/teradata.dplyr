5de590b4c62e5b856eede0b716c4c0edf4725ea6
ZJ <zhuojia.dai@gmail.com>
2015-02-24 11:17:17 +1100

    Update and rename R to R/teradata.query.r

---
+++ R/teradata.query.r
@@ -0,0 +1,52 @@
+Teradata.Query <- R6::R6Class("Teradata.Query",
+                     private = list(
+                       .nrow = NULL,
+                       .vars = NULL
+                     ),
+                     public = list(
+                       con = NULL,
+                       sql = NULL,
+                       
+                       initialize = function(con, sql, vars) {
+                         self$con <- con
+                         self$sql <- sql
+                         private$.vars <- vars
+                       },
+                       
+                       print = function(...) {
+                         cat("<Query> ", self$sql, "\n", sep = "")
+                         print(self$con)
+                       },
+                       
+                       fetch = function(n = 0) {
+                         res <- sqlQuery(self$con, self$sql, n)
+                         res
+                       },
+                       
+                       fetch_paged = function(chunk_size = 1e4, callback) {
+                         warning("This package does not support fetched_paged for Teradata")
+#                          qry <- dbSendQuery(self$con, self$sql)
+#                          on.exit(dbClearResult(qry))
+#                          
+#                          while (!dbHasCompleted(qry)) {
+#                            chunk <- fetch(qry, chunk_size)
+#                            callback(chunk)
+#                          }
+                         
+                         invisible(TRUE)
+                       },
+                       
+                       vars = function() {
+                         private$.vars
+                       },
+                       
+                       nrow = function() {
+                         count_rows_sql <- sprintf("SELECT CAST(COUNT(*) AS FLOAT) FROM (%s) as tmp;", self$sql)
+                         as.numeric(sqlQuery(self$con, count_rows_sql))
+                       },
+                       
+                       ncol = function() {
+                         length(self$vars())
+                       }
+                     )
+)
