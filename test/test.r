62130e5eaaed098ba2b821bb055272e761387fee
ZJ <zhuojia.dai@gmail.com>
2015-02-24 11:17:51 +1100

    Update test.r

--- test/test.r
+++ test/test.r
@@ -6,22 +6,23 @@ library("assertthat")
 
 #con <- tdConnect(dsn, uid = uid, pwd = pwd, database = database)
 #a <- td.data.frame(test_table)
-# tdQuery('select count(*) from airlines')
+# tdQuery('select count(*) from udparm.ao_sovn')
+
+#td.stats(a, "Delinquency")
 #tdClose()
 
-#a1 <- as.td.data.frame(a, tableName = "airlines", database = "")
+#a1 <- as.td.data.frame(a, tableName = "ao_sovn2", database = "udparm")
 
-st <- src_teradata(host = dsn, user = uid, password = pwd, dbname = "")
+st <- src_teradata(dsn = dsn, uid = uid, pwd = pwd)
 a <- tbl(st, "airlines")
+dim(a)
+collect(a)
+collapse(a)
+compute(a)
 
-copy_nycflights13(st)
-#copy_nycflights13(src_teradata(host = dsn, user = uid, password = pwd))
-#copy_lahman(src_teradata(host = dsn, user = uid, password = pwd))
+a1 <- filter(a, carrier == "AA")
+collect(a1)
+collapse(a1)
+compute(a1)
+dim(a1)
 
-getS3method("collapse","tbl_sql")
-
-a <- td.table(st$con,table = "airlines",database = "")
-
-collapse(select(a))
-
-a <- tbl(st, "airlines")
